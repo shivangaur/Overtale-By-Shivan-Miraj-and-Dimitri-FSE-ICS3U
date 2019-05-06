a68071776c538bc9ae6a885e58e8e43e95571acb
DimitriosChristopoulos <50279329+DimitriosChristopoulos@users.noreply.github.com>
2019-05-06 19:19:16 -0400

    Add files via upload

--- Overtale.py
+++ Overtale.py
@@ -1,109 +1,146 @@
+#importing what the program needs
 from pygame import *
 from random import *
 from math import *
-size=width,height=800,600
-screen=display.set_mode(size)
-RED=(255,0,0)   
-GREEN=(0,255,0)
-BLUE=(0,0,255)
-BLACK=(0,0,0)
-WHITE=(255,255,255)
+
+#making a screen
+size = width,height = 1000,750
+screen = display.set_mode(size)
+
+#making basic colors
+RED = (255,0,0)   
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+#declaring basic vraiables
+
+#declaring character's x and y position and making a list of them
+X = 0 #character's x position
+Y = 1 #character's y position
+#there is no "vy" or jumping power because there is no jumping in the game
+    #X  Y 
+guy = [X,Y]
+
+#initializing font
 font.init()
-comicSansFont=font.SysFont("Consolas",30)
+comicSansFont = font.SysFont("Consolas",40)#loading comic sans font
+
+#initializing sounds
 mixer.init()
-musicChannel=mixer.Channel(0)
+musicChannel = mixer.Channel(0)#making a mixer
+introMusic = mixer.Sound("Sound/introMusic/intro.ogg")
+musicChannel.play(introMusic)
 
-slides = []
+#making a clock
+myClock = time.Clock()
+myClock.tick(60)#making the program run 60 times per second
 
-def addSlide(text1, text2, picture, sound):
-    global slides
-    slides.append({"text1": text1, "text2": text2, "picture": picture, "sound": sound})
+#opening text files
+introText = open("text/intro/intro text.txt","r")#opening intro slides' text
+slideTexts = introText.readlines()#getting a list of each line(slide) of dialogue
 
-class slide:
-    def __init__(self,text1,text2,picture,sound):
-        self.text1=text1
-        self.text2=text2
-        self.picture=picture
-        self.sound=sound
-        
-        
-def drawSlide(text,text2,picture,sound):
-    screen.blit(picture,(100,100))
-    fx=140
-    fy=410
-    counter = 0
-    
-    for char in text:
-        inc=0
-        if char=="." or char=="," or char==":" or char==";":
-            inc=300
-        char=comicSansFont.render(char,True,WHITE)
-        screen.blit(char,(fx,fy))
-        fx+=13
-        display.flip()
-        if counter % 18 == 0:
-            musicChannel.play(firstIntro.sound)
-        counter += 1
-        time.wait(55+inc)
-    fx=140
-    fy+=40
-    for char in text2:
-        inc=0
-        if char=="." or char=="," or char==":" or char==";":
-            inc=300
-        char=comicSansFont.render(char,True,WHITE)
-        screen.blit(char,(fx,fy))
-        display.flip()
-        time.wait(55+inc)
-        fx+=13
-    screen.fill(BLACK)
-    mixer.pause()
-    time.wait(1000)
+#loading pictures
+backPic = image.load("Pictures/backPics/back1.png") #4000x500
+guyPic = image.load("Pictures/girlPics/guy.png")#29x31
 
-def drawPicture(picture):
-    screen.blit(picture,(100,100))
+#defining functions
+def drawSlides(text,picture):#defining a function that draws intro slides
+    screen.blit(picture,(100,100))#blitting the picture for the slide
+    fx = 100#font x position
+    fy = 550#font y position
+    count = 0#a counter that is used to make sure that the audio doesnt get cut off
+    for char in text:#for the character in the text
+        count+=1#increase the count by 1
+        if char == "~":#if the character is "~", this means that the program needs to start a new line. This is in the text files so that we can use .readlines() more easily, as we can fit everything that should be said in one text box in one line
+            fx = 100
+            fy+=50
+        elif char=="\n":
+            fx+=0
+        else:
+            inc = 0#increment so that letters appear 1 by 1, also reseting it so that the increment only increases for pauses
+            count+=1#counter increases by 1
+            if char == "." or char == ",":#if the character is punctuation
+                inc = 400#increase the amount of time the program waits after displaying the letter
+            char = comicSansFont.render(char,True,WHITE)#render the character
+            screen.blit(char,(fx,fy))#blit the font at font x and font y
+            display.flip()#show everything
+            time.wait(65+inc)#wait so that letters show 1 by 1
+            fx+=20#move the next character 13 pixels over
+    time.wait(1500)#at the end of the slide, wait 1500 millaseconds
+    screen.fill(BLACK)#fill the screen with black color so that the next slide doesnt overlap
+
+def drawPictures(picture):#defining function that will blit the picture of the slides
+    screen.blit(picture,(100,100))#blit the picture
     display.flip()
-    time.wait(3000)
-    screen.fill(BLACK)
+    time.wait(2500)#wait so that they user can see the picture
+    screen.fill(BLACK)#fill the screen with black color so that the next slide doesnt overlap
 
-class picture:
-    def __init__(self,picture):
-        self.picture=picture
+def checkPixelX(x,y,w,col):
+    '''returns the colour of the pixel at current (x,y)'''
+    cond = True
+    for i in range(w):
+        if screen.get_at((x+i,y)) == col:
+            cond = False
+    return cond
+    #return (-1,-1,-1)
 
-firstIntro=slide("Long ago, two races ruled over Earth:","HUMANS and MONSTERS.",image.load("Pictures/firstSlidePicture.png"),mixer.Sound("Sound/Sans talking test.ogg"))
-secondIntro=slide("One day, a war broke out between the","two races.",image.load("Pictures/secondSlidePicture.jpg"),firstIntro.sound)
-thirdIntro=slide("After a long battle, the humans were", "victorious.",image.load("Pictures/thirdSlidePicture.png"),firstIntro.sound)
-fourthIntro=slide("They sealed the monsters underground","with a magic spell.",thirdIntro.picture,firstIntro.sound)
-fifthIntro=slide("Many years later . . .","",image.load("Pictures/fifthSlidePicture.png"),firstIntro.sound)
-sixthIntro=slide("MT. Rumnis","2016",image.load("Pictures/sixthSlidePicture.jpg"),firstIntro.sound)
-seventhIntro=slide("Many people say that those who","climb the mountain never return.",image.load("Pictures/seventhSlidePicture.jpg"),firstIntro.sound)
-        
-firstPicture=picture(image.load("Pictures/eighthSlidePicture.jpg"))
-secondPicture=picture(image.load("Pictures/ninthSlidePicture.jpg"))
-thirdPicture=picture(image.load("Pictures/tenthSlidePicture.jpg"))
+def checkPixelY(x,y,h,col):
+    '''returns the colour of the pixel at current (x,y)'''
+    cond = True
+    for i in range(h):
+        if screen.get_at((x,y+i))== col:
+            cond = False
+    return cond
+
+def drawScene(screen,guy):
+    screen.blit(backPic,(-guy[X],-guy[Y]))
+    screen.blit(guyPic,(480,400))
+    display.flip()
     
+def moveGuy(guy):
+    keys = key.get_pressed()
+    if keys[K_LEFT] and guy[X]>-400:
+        if checkPixelY(479,400,guyPic.get_height(),BLACK):
+            guy[X]-=10
+    if keys[K_RIGHT] and guy[X]<2267:
+        if checkPixelY(532,400,guyPic.get_height(),BLACK):
+            guy[X]+=10
+    if keys[K_UP] and guy[Y]>-480:
+        if checkPixelX(480,399,guyPic.get_width(),BLACK):
+            guy[Y]-=10
+    if keys[K_DOWN] and guy[Y]<800:
+        if checkPixelX(480,481,guyPic.get_width(),BLACK):
+            guy[Y]+=10
 
-drawSlide(firstIntro.text1,firstIntro.text2,firstIntro.picture,firstIntro.sound)
-drawSlide(secondIntro.text1,secondIntro.text2,secondIntro.picture,firstIntro.sound)
-drawSlide(thirdIntro.text1,thirdIntro.text2,thirdIntro.picture,firstIntro.sound)
-drawSlide(fourthIntro.text1,fourthIntro.text2,fourthIntro.picture,firstIntro.sound)
-drawSlide(fifthIntro.text1,fifthIntro.text2,fifthIntro.picture,firstIntro.sound)
-drawSlide(sixthIntro.text1,sixthIntro.text2,sixthIntro.picture,firstIntro.sound)
-drawSlide(seventhIntro.text1,seventhIntro.text2,seventhIntro.picture,firstIntro.sound)
+for i in range(7):
+    drawSlides(slideTexts[i],image.load("Pictures/introPics/intro"+str(i+1)+".jpg"))#draw the slides 1 through 6, adding 1 because the for loop starts at 0 and ends at 5
+   
+for i in range(4):
+    drawPictures(image.load("Pictures/introPics/intro"+str(i+7)+".jpg"))#drawing the pictures 7 through 9,adding 7 because the pictures are labeled in the folder in order of the slides
 
-drawPicture(firstPicture.picture)
-drawPicture(secondPicture.picture)
-drawPicture(thirdPicture.picture)
+intro11 = image.load("Pictures/introPics/intro11.jpg")#loading final image, this does not get a function because it is simpler to just do it without one, as it is only one picture
+screen.blit(intro11,(200,-928))#blit the picture at 200,-627, as the picture is 924 tall, and we only want to show the bottom 297 pixels at this point so it matches the other pictures
+display.flip()
+time.wait(2000)#wait 2000 millaseconds
 
-
-running=True
+for i in range(1368):
+    screen.blit(intro11,(100,-928+i))#draw the picture 1 pixel lower
+    draw.rect(screen,BLACK,(100,440,800,400))#draw a rect below the picture so that when it starts coming down you only see 297 vertical pixels of picture
+    display.flip()#show all this
+time.wait(1000)
+musicChannel.pause()
+            
+running = True         
 while running:
-    for evt in event.get():
-        if evt.type==QUIT:
-            running=False
-                       
-    mb=mouse.get_pressed()
-    mx,my=mouse.get_pos()
+    for evnt in event.get():                
+        if evnt.type == QUIT:
+            running = False
+            
+    moveGuy(guy)
+    drawScene(screen,guy)
+    myClock.tick(60) 
 
 quit()
- 
+
