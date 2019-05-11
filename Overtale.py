6f38da098df90e7bf65d443551212757cf7490d4
DimitriosChristopoulos <50279329+DimitriosChristopoulos@users.noreply.github.com>
2019-05-11 17:09:07 -0400

    Add files via upload

--- Overtale.py
+++ Overtale.py
@@ -1,4 +1,3 @@
-
 #importing what the program needs
 from pygame import *
 from random import *
@@ -45,19 +44,19 @@ myClock.tick(60)#making the program run 60 times per second
 introText = open("text/intro/intro text.txt","r")#opening intro slides' text
 slideTexts = introText.readlines()#getting a list of each line(slide) of dialogue
 
-#loading pictures
-#backPic = image.load() #4000x500
-#backPic2= image.load()
 backPics=[]
 unloadedPics=["Pictures/backPics/back1.png",'Pictures/backPics/back2.png']
 for unload in unloadedPics:
     backPics.append(image.load(unload))
 guyPic = image.load("Pictures/guy/guy.png")#29x31
-
+heartPic = image.load("pictures/heart.png")
+HX = 475
+HY = 475
+heart = [HX,HY,100,100]
 
 #defining functions
 def drawSlides(text,picture):#defining a function that draws intro slides
-    screen.blit(picture,(100,100))#blitting the picture for the slide
+    screen.blit(picture,(175,100))#blitting the picture for the slide
     fx = 100#font x position
     fy = 550#font y position
     count = 0#a counter that is used to make sure that the audio doesnt get cut off
@@ -82,7 +81,7 @@ def drawSlides(text,picture):#defining a function that draws intro slides
     screen.fill(BLACK)#fill the screen with black color so that the next slide doesnt overlap
 
 def drawPictures(picture):#defining function that will blit the picture of the slides
-    screen.blit(picture,(100,100))#blit the picture
+    screen.blit(picture,(175,100))#blit the picture
     display.flip()
     time.wait(2500)#wait so that they user can see the picture
     screen.fill(BLACK)#fill the screen with black color so that the next slide doesnt overlap
@@ -104,20 +103,7 @@ def checkPixelY(x,y,h,col):
         if screen.get_at((x,y+i))== col:
             
             cond = False
-    return cond
-def floweyScene(guy,picsList):
-    frame=0
-    guy[X]=0
-    guy[Y]=0
-    guy[2]=480
-    guy[3]=500
-    print(guy[3])
-    while guy[3]>480:
-       # frame+=0.2
-        guy[3]-=1
-    screen.blit(picsList[2][int(frame)],(guy[2],guy[3]))
-    
-        
+    return cond        
         
 def addPics(name,start,end):
     '''this function will return a LIST OF PICTURES
@@ -129,33 +115,6 @@ def addPics(name,start,end):
         myPics.append(image.load("Pictures/%s/%s%03d.png"%(name,name,i)))
     return myPics
 
-def drawScene(screen,picsList,backPics,guy):
-    #print(S)
-    global S
-    if guy[X]<=-190 and guy[Y]<=-50:
-        screen.blit(backPics[S],(190,50))
-    elif 1510<guy[X]<1620 and guy[Y]<=41:
-        S+=1
-        floweyScene(guy,picsList)
-        #screen.blit(backPics[S],(190,50))
-        #screen.blit(backPics[S],(0,0))
-        #draw.rect(screen,BLACK,(0,0,width,height))
-    elif guy[X]<=-190:
-        screen.blit(backPics[S],(190,-guy[Y]))
-    
-
-    elif guy[X]>=1220:
-        screen.blit(backPics[S],(-1220,-guy[Y]))
-    elif guy[Y]<=-50:
-        screen.blit(backPics[S],(-guy[X],50))
-    
-    else:
-        screen.blit(backPics[S],(-guy[X],-guy[Y]))
-    #print(guy[X],guy[Y],guy[2],guy[3])
-   # print(guy)
-    screen.blit(picsList[move][int(frame)],(guy[2],guy[3]))
-    display.flip()
-    
 def moveGuy(guy):
     global move,frame
     newMove=-1
@@ -166,39 +125,24 @@ def moveGuy(guy):
                 guy[2]-=10
             if guy[X]>=1220:
                 guy[2]-=10
-
             guy[X]-=10
             newMove=3
-            #else:
-            
-                #guy[X]-=10
-                #newMove=3
-            
     elif keys[K_RIGHT] and guy[X]<2267:
         if checkPixelY(guy[2]+52,guy[3],guyPic.get_height(),BLACK):
-            
             if guy[X]<=-190:
                 guy[2]+=10
             if guy[X]>=1220:
                 guy[2]+=10
             guy[X]+=10
             newMove=0
-            
-            
-            
     elif keys[K_UP] and guy[Y]>-480:
         if checkPixelX(guy[2],guy[3]-1,guyPic.get_width(),BLACK):
             if guy[Y]<=-50:
                 guy[3]-=10
-            #if guy[X]<=-190:
-             #    guy[Y]-=10   
-            
             guy[Y]-=10
             newMove=2
-            
     elif keys[K_DOWN] and guy[Y]<800:
         if checkPixelX(guy[2],guy[3]+81,guyPic.get_width(),BLACK):
-            
             if guy[Y]<=-50:
                 guy[3]+=10
             guy[Y]+=10
@@ -213,28 +157,45 @@ def moveGuy(guy):
         move=newMove
         frame=1
 
-##for i in range(8):
-##    drawSlides(slideTexts[i],image.load("Pictures/introPics/intro"+str(i+1)+".jpg"))#draw the slides 1 through 6, adding 1 because the for loop starts at 0 and ends at 5
-##   
-##for i in range(3):
-##    drawPictures(image.load("Pictures/introPics/intro"+str(i+8)+".jpg"))#drawing the pictures 7 through 9,adding 7 because the pictures are labeled in the folder in order of the slides
-##
-##intro11 = image.load("Pictures/introPics/intro11.jpg")#loading final image, this does not get a function because it is simpler to just do it without one, as it is only one picture
-##screen.blit(intro11,(100,-828))#blit the picture at 100,-928, as the picture is 1368 tall, and we only want to 500 pixels at this point so it matches the other pictures
-##draw.rect(screen,BLACK,(100,0,800,100))
-##display.flip()
-##time.wait(2000)#wait 2000 millaseconds
-##
-##for i in range(828):
-##    screen.blit(intro11,(100,-828+i))#draw the picture 1 pixel lower
-##    draw.rect(screen,BLACK,(100,0,800,100))
-##    draw.rect(screen,BLACK,(100,540,800,400))#draw a rect below the picture so that when it starts coming down you only see 297 vertical pixels of picture
-##    display.flip()#show all this
-##time.wait(10000)
-##musicChannel.pause()
+def drawScene(screen,picsList,backPics,guy):
+    global S #what scene the character is in
+    #if gateRect.collideRect(guy):#if they are moving to the next level
+        #S+=1#mvoe to the next scene
+    if guy[X]<=-190 and guy[Y]<=-50:#the rest of the lines are chekcing where the character is, because if they are too far into one part of the scene we want to move the CHARACTER and NOT the background picture
+        screen.blit(backPics[S],(190,50))
+    elif guy[X]<=-190:
+        screen.blit(backPics[S],(190,-guy[Y]))
+    elif guy[X]>=1220:
+        screen.blit(backPics[S],(-1220,-guy[Y]))
+    elif guy[Y]<=-50:
+        screen.blit(backPics[S],(-guy[X],50))
+    else:
+        screen.blit(backPics[S],(-guy[X],-guy[Y]))
+    screen.blit(picsList[move][int(frame)],(guy[2],guy[3]))
+    display.flip()
+    
 
-pics=[addPics('guy',1,3),
-    addPics("guy",4,6),addPics('guy',7,9),addPics('guy',10,12)]            
+for i in range(8):
+    if i!=7:
+        drawSlides(slideTexts[i],image.load("Pictures/introPics/intro"+str(i+1)+".jpg"))#draw the slides 1 through 6, adding 1 because the for loop starts at 0 and ends at 5
+   
+for i in range(3):
+    drawPictures(image.load("Pictures/introPics/intro"+str(i+8)+".jpg"))#drawing the pictures 7 through 9,adding 7 because the pictures are labeled in the folder in order of the slides
+
+intro11 = image.load("Pictures/introPics/intro11.jpg")#loading final image, this does not get a function because it is simpler to just do it without one, as it is only one picture
+screen.blit(intro11,(175,-550))#blit the picture at 100,-550, as the picture is 1368 tall, and we only want to 500 pixels at this point so it matches the other pictures
+draw.rect(screen,BLACK,(175,0,800,100))
+display.flip()
+time.wait(2000)#wait 2000 millaseconds
+
+for i in range(550):
+    screen.blit(intro11,(175,-550+i))#draw the picture 1 pixel lower
+    draw.rect(screen,BLACK,(175,0,800,100))
+    draw.rect(screen,BLACK,(175,550,800,400))#draw a rect below the picture so that when it starts coming down you only see 297 vertical pixels of picture
+    display.flip()#show all this
+time.wait(5000)
+musicChannel.pause()
+
 running = True         
 while running:
     for evnt in event.get():                
@@ -242,7 +203,7 @@ while running:
             running = False
             
     moveGuy(guy)
-    drawScene(screen,pics,backPics,guy)
+    drawScene(screen,myPics,backPics,guy)
     myClock.tick(60)
 
 quit()
