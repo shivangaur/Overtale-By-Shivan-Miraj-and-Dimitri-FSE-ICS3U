599fde24011c3d58ecfd55f9afa219931e5e3f8a
DimitriosChristopoulos <50279329+DimitriosChristopoulos@users.noreply.github.com>
2019-05-15 23:44:27 -0400

    Add files via upload

--- floweyintro.py
+++ floweyintro.py
@@ -1,7 +1,7 @@
-
 from pygame import *
 from random import *
 from math import *
+writing = True
 size = width,height = 1000,750
 screen=display.set_mode(size)
 RED=(255,0,0)   
@@ -10,86 +10,145 @@ BLUE=(0,0,255)
 BLACK=(0,0,0)
 WHITE=(255,255,255)
 font.init()
-comicSansFont = font.SysFont("Consolas",30)
+consolas = font.SysFont("Consolas",30)
 mixer.init()
 musicChannel = mixer.Channel(0)
 musicChannel2 = mixer.Channel(1)
 floweySound = mixer.Sound("Sound/characters/flowey.ogg")
 floweyTheme = mixer.Sound("Sound/themes/flowey.ogg")
-
-heart=image.load("Pictures/heart.png")
-normalFlowey = image.load("Pictures/flowey/flowey.jpg")
 X,Y=0,1
+heart = [475,600,50,50]
+heartRect = Rect(heart[X],heart[Y],50,50)
+heartPic=image.load("Pictures/heart.png")
+normalFlowey = image.load("Pictures/flowey/flowey.jpg")
 guy=[X,Y,480,400]
+heart=[460,450,50,50]
 floweyCombat = image.load("Pictures/combat/floweyfight.jpg")
 floweyText = open("combattext/flowey/floweyintro.txt")
 floweyTalking = image.load("Pictures/flowey/floweyTalking.jpg")
 textBox = image.load("Pictures/combat/text.jpg")
-script = floweyText.readlines()
+floweyScript = floweyText.readlines()
 
-guy = image.load("Pictures/guy/guy009.png")
+def addPics(name,start,end):
+    '''this function will return a LIST OF PICTURES
+    They must be in a folder named 'name' and all the pictures must start with 'name'
+    start,end-the range of picture numbers
+    '''
+    myPics=[]
+    for i in range(start,end+1):
+        myPics.append(image.load("Pictures/%s/%s%03d.png"%(name,name,i)))
+    return myPics
 
-def floweyIntro():
+pics=[addPics('guy',1,3),addPics("guy",4,6),addPics('guy',7,9),addPics('guy',10,12)]
+
+def combatIntro():
     for i in range(2):
         screen.fill(BLACK)
         display.flip()
         time.wait(150)
-        screen.blit(guy,(guy[2],guy[3]))
-#        screen.blit(pics[2][2],(guy[2],guy[3]))
+        screen.blit(pics[2][2],(guy[2],guy[3]))
         display.flip()
         time.wait(150)
-    #musicChannel2.play(floweyTheme)
-    screen.blit(floweyCombat,(0,0))
-    screen.blit(normalFlowey,(389,155))
     display.flip()
+    
+def drawPellets():
+    for i in range(50,250,5):
+        ellipseRect1=Rect(485-i,130,20,20)
+        ellipseRect2=Rect(485+i,130,20,20)
+        draw.ellipse(screen,WHITE,(ellipseRect1),10)
+        draw.ellipse(screen,WHITE,(ellipseRect2),10)
+        screen.blit(floweyCombat,(0,0))
+        screen.blit(textBox,(600,175))
+        screen.blit(normalFlowey,(389,155))
+        display.flip()
+    for i in range(50,80,5):
+        ellipseRect3=Rect(485-2*i,130-i,20,20)
+        ellipseRect4=Rect(485+2*i,130-i,20,20)
+        ellipseRect5=Rect(485,150-i-50,20,20)
+        draw.ellipse(screen,WHITE,(ellipseRect3),10)
+        draw.ellipse(screen,WHITE,(ellipseRect4),10)
+        draw.ellipse(screen,WHITE,(ellipseRect5),10)
+        display.flip()
+        time.wait(10)
+        screen.blit(floweyCombat,(0,0))
+        screen.blit(textBox,(600,175))
+        screen.blit(normalFlowey,(389,155))
+        display.flip()
 
-def drawFont(text):
+def floweyFont(text):
+    screen.blit(floweyCombat,(0,0))
+    screenshotVar = False
     screen.blit(textBox,(600,175))
     fx = 650
     fy = 195
     count = 0
-#    if text == script[8]:
+    if text == floweyScript[1]:
+        drawPellets()
     for char in text:
-#        if not musicChannel2.get_busy():
-#            musicChannel2.play(floweyTheme)
-        if count%6 == 0:
-            screen.blit(floweyTalking,(389,155))
-        else:
-            screen.blit(normalFlowey,(389,155))
         inc = 0
+        if screenshotVar:
+            screen.blit(screenshot,(600,175))
         if char == "\n":
-            fx=fx
+            fx = fx
         elif char == "~":
-            fx=650
+            fx = 650
             fy+=35
         else:
             if char == "," or char == "." or char == "!": 
                 inc = 400
-            char = comicSansFont.render(char,True,BLACK)
+            char = consolas.render(char,True,BLACK)
             screen.blit(char,(fx,fy))
+            if not musicChannel2.get_busy():
+                musicChannel2.play(floweyTheme)
+            if count%6 == 0:
+                screen.blit(floweyTalking,(389,155))
+            else:
+                screen.blit(normalFlowey,(389,155))
+            if count%15==0:
+                musicChannel.play(floweySound)
             display.flip()
-            #if count%30==0:
-                #musicChannel.play(floweySound)
             count+=1
             fx+=15
+            screenshot = screen.copy().subsurface(600,175,362,171)
+            screenshotVar = True
             time.wait(65+inc)
-    time.wait(3000)
+    time.wait(1500)
     
-floweyIntro()
-time.wait(300)
-for i in script:
-    drawFont(i)
+def moveHeart():
+    global heart
+    keys = key.get_pressed()
+    if (keys[K_a] or keys[K_LEFT]):
+        if heart[X]>=405:
+            heart[X]-=5
+    if (keys[K_d] or keys[K_RIGHT]):
+        if heart[X]<=795:
+            heart[X]+=5
+    if (keys[K_w] or keys[K_UP]):
+        if heart[Y]>=505:
+            heart[Y]-=5
+    if (keys[K_s] or keys[K_DOWN]):
+        if heart[Y]<=695:
+            heart[Y]+=5
 
+combat = True
+flowey = True
 running=True
 while running:
     for evt in event.get():
         if evt.type==QUIT:
             running=False
 
-    
+    if combat:
+        if flowey:
+            flowey = False
+            combatIntro()
+            musicChannel2.play(floweyTheme)
+            for text in floweyScript:
+                floweyFont(text)
+        moveHeart()
+        
     mb=mouse.get_pressed()
     mx,my=mouse.get_pos()
-    print(mx,my)
 floweyText.close()
 quit()
  
