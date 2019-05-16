bd4f2d275288782a168bc71207f7e547155d1089
DimitriosChristopoulos <50279329+DimitriosChristopoulos@users.noreply.github.com>
2019-05-16 10:57:22 -0400

    Add files via upload

--- floweyintro.py
+++ floweyintro.py
@@ -16,6 +16,7 @@ musicChannel = mixer.Channel(0)
 musicChannel2 = mixer.Channel(1)
 floweySound = mixer.Sound("Sound/characters/flowey.ogg")
 floweyTheme = mixer.Sound("Sound/themes/flowey.ogg")
+pellets = False
 X,Y=0,1
 heart = [475,600,50,50]
 heartRect = Rect(heart[X],heart[Y],50,50)
@@ -27,6 +28,7 @@ floweyCombat = image.load("Pictures/combat/floweyfight.jpg")
 floweyText = open("combattext/flowey/floweyintro.txt")
 floweyTalking = image.load("Pictures/flowey/floweyTalking.jpg")
 textBox = image.load("Pictures/combat/text.jpg")
+fightBox = image.load("Pictures/combat/fightBox.png")
 floweyScript = floweyText.readlines()
 
 def addPics(name,start,end):
@@ -49,6 +51,7 @@ def combatIntro():
         screen.blit(pics[2][2],(guy[2],guy[3]))
         display.flip()
         time.wait(150)
+    screen.blit(floweyCombat,(0,0))
     display.flip()
     
 def drawPellets():
@@ -57,11 +60,14 @@ def drawPellets():
         ellipseRect2=Rect(485+i,130,20,20)
         draw.ellipse(screen,WHITE,(ellipseRect1),10)
         draw.ellipse(screen,WHITE,(ellipseRect2),10)
+        display.flip()
+        screenshot = screen.copy()
         screen.blit(floweyCombat,(0,0))
         screen.blit(textBox,(600,175))
         screen.blit(normalFlowey,(389,155))
         display.flip()
     for i in range(50,80,5):
+        screen.blit(screenshot,(0,0))
         ellipseRect3=Rect(485-2*i,130-i,20,20)
         ellipseRect4=Rect(485+2*i,130-i,20,20)
         ellipseRect5=Rect(485,150-i-50,20,20)
@@ -69,25 +75,26 @@ def drawPellets():
         draw.ellipse(screen,WHITE,(ellipseRect4),10)
         draw.ellipse(screen,WHITE,(ellipseRect5),10)
         display.flip()
-        time.wait(10)
-        screen.blit(floweyCombat,(0,0))
         screen.blit(textBox,(600,175))
         screen.blit(normalFlowey,(389,155))
         display.flip()
-
+    screenshot = screen.copy()
+    pellets = True
+    
 def floweyFont(text):
-    screen.blit(floweyCombat,(0,0))
     screenshotVar = False
     screen.blit(textBox,(600,175))
     fx = 650
     fy = 195
     count = 0
-    if text == floweyScript[1]:
+    if text == floweyScript[7]:
         drawPellets()
     for char in text:
+        if pellets:
+            screen.blit(screenshot,(0,0))
         inc = 0
         if screenshotVar:
-            screen.blit(screenshot,(600,175))
+            screen.blit(screenshot2,(600,175))
         if char == "\n":
             fx = fx
         elif char == "~":
@@ -109,9 +116,10 @@ def floweyFont(text):
             display.flip()
             count+=1
             fx+=15
-            screenshot = screen.copy().subsurface(600,175,362,171)
+            screenshot2 = screen.copy().subsurface(600,175,362,171)
             screenshotVar = True
             time.wait(65+inc)
+    screenshot = screen.copy()
     time.wait(1500)
     
 def moveHeart():
@@ -129,6 +137,10 @@ def moveHeart():
     if (keys[K_s] or keys[K_DOWN]):
         if heart[Y]<=695:
             heart[Y]+=5
+            
+def drawFight():
+    screen.blit(fightBox,(500,600))
+    screen.blit(heartPic,(heart[X],heart[Y]))
 
 combat = True
 flowey = True
@@ -146,6 +158,7 @@ while running:
             for text in floweyScript:
                 floweyFont(text)
         moveHeart()
+        drawFight()
         
     mb=mouse.get_pressed()
     mx,my=mouse.get_pos()
